e96532894719ba738261ea7c0d195e83e02daf10
Raghav267 <harshraghav089@gmail.com>
2024-06-17 21:30:38 +0530

    Array String Math functions

---
+++ Lectur4/app.js
@@ -0,0 +1,14 @@
+// const age = prompt("Enter yoy age");
+
+// if (age > 18) {
+//     console.log("you are adult");
+// }
+// else {
+//     console.log("you are underage");
+//     alert("Go to homem kid")
+// }
+
+
+
+
+/// > < == ===
