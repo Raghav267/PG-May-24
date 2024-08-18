558f977d047e8b516cf6cc57796d38411c2fafab
Raghav267 <harshraghav089@gmail.com>
2024-08-18 19:54:08 +0530

    Shadwing and Discussion

---
+++ Lecture-27/index.js
@@ -0,0 +1,18 @@
+// Shadowing Concept\\
+
+// var a = 100;
+// {
+//     var a = 200;
+//     console.log(a);
+// }
+
+// console.log(a);
+
+
+let a = 100;
+{
+    let a = 200;
+    console.log(a);
+}
+
+console.log(a);
